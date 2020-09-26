58eadbac372307a713e3f75306146790a331d8d3
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-09-26 01:23:03 +0100

    Remove local zip after unzipping

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,8 @@ cd /build
 # install deps to run make
 dnf install --assumeyes make unzip git ansible
 
-unzip head.zip
+# open the package from the local repo and remove the zip
+unzip head.zip && rm head.zip
 
 # make the image and push it
 make playbook
